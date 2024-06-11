9f32c088e1388250556cff847194af75607a3ac2
youssefrafat257 <146446248+youssefrafat257@users.noreply.github.com>
2024-06-12 02:00:40 +0300

    Update Sidebar.js

--- src/Components/pages/Sidebar.js
+++ src/Components/pages/Sidebar.js
@@ -1,20 +1,33 @@
 // src/Sidebar.js
-import React from 'react';
-import studentImage from '../../assets/images/author/author-04.jpg';
+import React from "react";
+import studentImage from "../../assets/images/author/author-04.jpg";
 // import "../../Styles/Contact.css";
-import './Sidebar.css'
+import "./Sidebar.css";
 const Sidebar = () => {
   return (
-    <div className="sidebar-1" >
-<div style={{marginTop:'10px'}}> <img className="img1" src={studentImage} alt="Student" /></div>
-     <h4  >Cristiano Ronaldo</h4 >
+    <div className="sidebar-1">
+      <div style={{ marginTop: "10px" }}>
+        {" "}
+        <img className="img1" src={studentImage} alt="Student" />
+      </div>
+      <h4>Cristiano Ronaldo</h4>
 
       <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="courses">Courses</a></li>
-        <li><a href="about">About</a></li>
-        <li><a href="contact">Contact</a></li>
-        <li><a href="login">Logout</a></li>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="courses">Courses</a>
+        </li>
+        <li>
+          <a href="about">About</a>
+        </li>
+        <li>
+          <a href="contact">Contact</a>
+        </li>
+        <li>
+          <a href="login">Logout</a>
+        </li>
       </ul>
     </div>
   );
