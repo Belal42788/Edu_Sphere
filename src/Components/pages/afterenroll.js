d99361a1aaa26cdec8a6f88bd3ef0bcddd686753
Ahmed-Maher-166 <115505480+Ahmed-Maher-166@users.noreply.github.com>
2024-06-12 23:29:52 +0300

    afterenrollwith1mistake

--- src/Components/pages/afterenroll.js
+++ src/Components/pages/afterenroll.js
@@ -5,6 +5,9 @@ import author6 from "../../assets/images/author/author-06.jpg";
 import author7 from "../../assets/images/author/author-07.jpg";
 import Footer from "../Footer";
 import Navbar from "../Navbar";
+import author1 from "../../assets/images/author/author-01.jpg";
+import author2 from "../../assets/images/author/author-02.jpg";
+
 import Advertise from "../advertise";
 import "../../Styles/plugins/icofont.min.css";
 import "../../Styles/plugins/animate.min.css";
@@ -16,6 +19,76 @@ import "../../Styles/plugins/jqvmap.min.css";
 import "../../Styles/Contact.css";
 import AdminHeader from "../AdminHeader";
 function Afterenroll() {
+    const videoLinks = [
+        {
+          title: "01. The Complete Medicine Masterclass",
+          url: "https://player.vimeo.com/external/215175080.hd.mp4?s=5b17787857fd95646e67ad0f666ea69388cb703c&profile_id=119",
+          duration: "08 minutes"
+        },
+        {
+          title: "02. Standard dummy text ever since the",
+          url: "https://player.vimeo.com/external/207590826.hd.mp4?s=6a918d074abf8f3add7858018855524d384f6934&profile_id=119",
+          duration: "08 minutes"
+        },
+        {
+          title: "03. Printer took a galley of type and scrambled",
+          url: "https://player.vimeo.com/external/207590786.hd.mp4?s=bcd80c5d2f6bf1bbad3b1a670ef93861e72f9683&profile_id=119",
+          duration: "08 minutes"
+        },
+        {
+          title: "04. It to make a type specimen book & break",
+          url: "https://player.vimeo.com/external/207590615.hd.mp4?s=c4bce1872859889c86b688c26c60ed2b5734de28&profile_id=119",
+          duration: "08 minutes"
+        },
+        {
+          title: "05. Survived not only five centuries also the leap",
+          url: "http://download.wavetlan.com/SVV/Media/HTTP/H264/Other_Media/H264_test5_voice_mp4_480x360.mp4",
+          duration: "08 minutes"
+        },
+        {
+          title: "06. Into typesettingremaining essentially",
+          url: "http://download.wavetlan.com/SVV/Media/HTTP/H264/Other_Media/H264_test7_voiceclip_mp4_480x360.mp4",
+          duration: "08 minutes"
+        },
+        {
+          title: "07. Unchanged. It was popularised in the 1960s",
+          url: "http://download.wavetlan.com/SVV/Media/HTTP/H264/Other_Media/H264_test8_voiceclip_mp4_480x320.mp4",
+          duration: "08 minutes"
+        },
+        {
+          title: "08. The release of Letraset sheets containing",
+          url: "http://download.wavetlan.com/SVV/Media/HTTP/MP4/ConvertedFiles/Media-Convert/Unsupported/dw11222.mp4",
+          duration: "08 minutes"
+        }
+      ];
+      const coursesinf = [
+        {
+          id: 1, title: 'Data Science and Machine Learning with Python - Hands On!', instructor: 'John Doe', lecture: '29', price: '428' ,Language:'English',
+          Certificate:'Yes'}]
+          const teamInfo = [
+            {
+                id: 1,
+                name: 'Margarita James',
+                designation: 'MSC, Instructor',
+                rating: 4.9,
+                ImageUrl: author1
+            },
+            {
+                id: 2,
+                name: 'Mitchell Colon',
+                designation: 'BBA, Instructor',
+                rating: 4.9,
+                ImageUrl: author2
+            },
+            {
+                id: 3,
+                name: 'Sonya Gordon',
+                designation: 'MBA, Instructor',
+                rating: 4.9,
+                ImageUrl: author3
+            },
+         
+        ];   
 return (
 <>
 <div>
@@ -117,32 +190,29 @@ return (
                                                         popularsed in the 1960 with containing Lorem Ipsum passages
                                                         desktop publishing software.</p>
                                                     <table class="table">
+                                                    {coursesinf.map((item)=>(
                                                         <tbody>
+                                                       
                                                             <tr>
                                                                 <th>Instructor <span>:</span></th>
-                                                                <td>Pamela Foster</td>
+                                                                <td>{item.instructor}</td>
                                                             </tr>
-                                                            <tr>
-                                                                <th>Duration <span>:</span></th>
-                                                                <td>08 hr 15 mins</td>
-                                                            </tr>
+                                                         
                                                             <tr>
                                                                 <th>Lectures <span>:</span></th>
-                                                                <td>2,16</td>
+                                                                <td>{item.lecture}</td>
                                                             </tr>
-                                                            <tr>
-                                                                <th>Level <span>:</span></th>
-                                                                <td>Secondary</td>
-                                                            </tr>
+                                                         
                                                             <tr>
                                                                 <th>Language <span>:</span></th>
-                                                                <td>English</td>
+                                                                <td>{item.Language}</td>
                                                             </tr>
                                                             <tr>
                                                                 <th>Caption’s <span>:</span></th>
-                                                                <td>Yes</td>
+                                                                <td>{item.Certificate}</td>
                                                             </tr>
                                                         </tbody>
+                                                    ))}
                                                     </table>
                                                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting
                                                         industry. Lorem Ipsum has been industry's standard dummy text
@@ -219,22 +289,27 @@ return (
                                 <div class="tab-pane fade" id="tab4">
                                     <div class="instructor">
                                         <div class="row">
+                                       
                                             <div class="col-lg-4">
                                                 <div class="enroll-tab-title">
                                                     <h3 class="title">Instructor</h3>
                                                 </div>
                                             </div>
+                                            
                                             <div class="col-lg-8">
                                                 <div class="enroll-tab-content">
+                                                {teamInfo.map((item)=>(
+
+
                                                     <div class="single-instructor">
                                                         <div class="review-author">
                                                             <div class="author-thumb">
-                                                                <img src={author7}
+                                                                <img src={item.ImageUrl}
                                                                     alt="Author"/>
                                                             </div>
                                                             <div class="author-content">
-                                                                <h4 class="name">Karol Alexander</h4>
-                                                                <span class="designation">Product Designer, USA</span>
+                                                                <h4 class="name">{item.name}</h4>
+                                                                <span class="designation">Product Designer, {item.designation}</span>
                                                                 <span class="rating-star">
                                                                     <span class="rating-bar" style={{width: '80%'}}></span>
                                                                 </span>
@@ -247,54 +322,17 @@ return (
                                                             specimen book.</p>
                                                     </div>
                                             
-                                                    <div class="single-instructor">
-                                                        <div class="review-author">
-                                                            <div class="author-thumb">
-                                                                <img src={author7}
-                                                                    alt="Author"/>
-                                                            </div>
-                                                            <div class="author-content">
-                                                                <h4 class="name">Karol Bachman</h4>
-                                                                <span class="designation">Product Designer, USA</span>
-                                                                <span class="rating-star">
-                                                                    <span class="rating-bar" style={{width: '80%'}}></span>
-                                                                </span>
-                                                            </div>
-                                                        </div>
-                                                        <p>Lorem Ipsum is simply dummy text of the printing and
-                                                            typesetting industry. Lorem Ipsum has been industry's
-                                                            standard dummy text ever since the 1500s when andom unknown
-                                                            printer took a galley of type scrambled it to make a type
-                                                            specimen book.</p>
-                                                    </div>
-                                           
-                                                    <div class="single-instructor">
-                                                        <div class="review-author">
-                                                            <div class="author-thumb">
-                                                                <img src={author7}
-                                                                    alt="Author"/>
-                                                            </div>
-                                                            <div class="author-content">
-                                                                <h4 class="name">Gertude Culbertson</h4>
-                                                                <span class="designation">Product Designer, USA</span>
-                                                                <span class="rating-star">
-                                                                    <span class="rating-bar" style={{width: '80%'}}></span>
-                                                                </span>
-                                                            </div>
-                                                        </div>
-                                                        <p>Lorem Ipsum is simply dummy text of the printing and
-                                                            typesetting industry. Lorem Ipsum has been industry's
-                                                            standard dummy text ever since the 1500s when andom unknown
-                                                            printer took a galley of type scrambled it to make a type
-                                                            specimen book.</p>
-                                                    </div>
-                                                  
+                                                 
+                                        ))}
                                                 </div>
+                                         
                                             </div>
+                                        
                                         </div>
                                     </div>
                                  
                                 </div>
+                            
                             </div>
                         </div>
                     </div> 
@@ -312,46 +350,14 @@ return (
                                 <div id="collapseOne"
                                     data-bs-parent="#videoPlaylist">
                                     <nav class="vids">
+                                        {videoLinks.map((item)=>(
                                         <a class="link active"
-                                            href="https://player.vimeo.com/external/215175080.hd.mp4?s=5b17787857fd95646e67ad0f666ea69388cb703c&amp;profile_id=119">
-                                            <p>01. The Complete Medicine Masterclass</p>
-                                            <span class="total-duration">08 minutes</span>
+                                            href={item.url}>
+                                            <p>{item.title}</p>
+                                            <span class="total-duration"> {item.duration}</span>
                                         </a>
-                                        <a class="link"
-                                            href="https://player.vimeo.com/external/207590826.hd.mp4?s=6a918d074abf8f3add7858018855524d384f6934&amp;profile_id=119">
-                                            <p>02. Standard dummy text ever since the</p>
-                                            <span class="total-duration">08 minutes</span>
-                                        </a>
-                                        <a class="link"
-                                            href="https://player.vimeo.com/external/207590786.hd.mp4?s=bcd80c5d2f6bf1bbad3b1a670ef93861e72f9683&amp;profile_id=119">
-                                            <p>03. Printer took a galley of type and scrambled</p>
-                                            <span class="total-duration">08 minutes</span>
-                                        </a>
-                                        <a class="link"
-                                            href="https://player.vimeo.com/external/207590615.hd.mp4?s=c4bce1872859889c86b688c26c60ed2b5734de28&amp;profile_id=119">
-                                            <p>04. It to make a type specimen book & break</p>
-                                            <span class="total-duration">08 minutes</span>
-                                        </a>
-                                        <a class="link"
-                                            href="http://download.wavetlan.com/SVV/Media/HTTP/H264/Other_Media/H264_test5_voice_mp4_480x360.mp4">
-                                            <p>05. Survived not only five centuries also the leap</p>
-                                            <span class="total-duration">08 minutes</span>
-                                        </a>
-                                        <a class="link"
-                                            href="http://download.wavetlan.com/SVV/Media/HTTP/H264/Other_Media/H264_test7_voiceclip_mp4_480x360.mp4">
-                                            <p>06. Into typesettingremaining essentially</p>
-                                            <span class="total-duration">08 minutes</span>
-                                        </a>
-                                        <a class="link"
-                                            href="http://download.wavetlan.com/SVV/Media/HTTP/H264/Other_Media/H264_test8_voiceclip_mp4_480x320.mp4">
-                                            <p>07. Unchanged. It was popularised in the 1960s</p>
-                                            <span class="total-duration">08 minutes</span>
-                                        </a>
-                                        <a class="link"
-                                            href="http://download.wavetlan.com/SVV/Media/HTTP/MP4/ConvertedFiles/Media-Convert/Unsupported/dw11222.mp4">
-                                            <p>08. The release of Letraset sheets containing</p>
-                                            <span class="total-duration">08 minutes</span>
-                                        </a>
+                                        ))}
+                                    
                                     </nav>
                                 </div>
                             </div>
